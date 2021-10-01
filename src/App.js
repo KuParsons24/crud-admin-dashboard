91f8274394d5488a07213ff2f8cb8bf292b5371f
Kurtis Parsons <kuparsons24@gmail.com>
2021-09-30 21:31:05 -0400

    updated react app location on host server

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
+    <Router basename='/crud'>
       <Layout>
         <Switch>
           <Route exact path='/'>
